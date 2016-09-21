46dc800b34c55a531b13e32263645b0f7755f4eb
이동현 <wracker12@hanmail.net>
2016-09-22 01:27:01 +0900

    commit

---
+++ hPig/app/controllers/MyInfoController.swift
@@ -0,0 +1,35 @@
+//
+//  MyInfoController.swift
+//  hPig
+//
+//  Created by 이동현 on 2016. 9. 21..
+//  Copyright © 2016년 wearespeakingtube. All rights reserved.
+//
+
+import UIKit
+
+class MyInfoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ hPig/app/models/Session.swift
@@ -0,0 +1,9 @@
+//
+//  Session.swift
+//  hPig
+//
+//  Created by 이동현 on 2016. 9. 21..
+//  Copyright © 2016년 wearespeakingtube. All rights reserved.
+//
+
+import Foundation
