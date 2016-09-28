e08ead0df3622016f90dd7fb80fb58c236325327
이동현 <wracker12@hanmail.net>
2016-09-29 01:53:20 +0900

    네이버 로그인 추가

---
+++ hPig-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "NaverThirdPartyConstantsForApp.h"
+#import "NaverThirdPartyLoginConnection.h"
+#import "NLoginThirdPartyOAuth20InAppBrowserViewController.h"
--- hPig/AppDelegate.swift
+++ hPig/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-
+                   
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        NaverThirdPartyLoginConnection.getSharedInstance().isNaverAppOauthEnable = true
+        NaverThirdPartyLoginConnection.getSharedInstance().isInAppOauthEnable = true
+        
         return true
     }
 
