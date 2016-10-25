e6a5bcd57e63a1301c31ae4fe7abfd66b3b5242d
jesse.lee <jesse.lee@kakaocorp.com>
2016-10-25 18:18:29 +0900

    개발중

---
+++ hPig/app/views/cells/sessions/SessionFeedCell.swift
@@ -0,0 +1,17 @@
+//
+//  SessionFeedCell.swift
+//  hPig
+//
+//  Created by Jesse on 2016. 10. 25..
+//  Copyright © 2016년 wearespeakingtube. All rights reserved.
+//
+
+import UIKit
+
+class SessionFeedCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
