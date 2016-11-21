af95525c320a4ac2aa64947c687ef1203e3172e3
jesse.lee <jesse.lee@kakaocorp.com>
2016-11-21 18:04:38 +0900

    개발중

--- hPig/AppDelegate.swift
+++ hPig/AppDelegate.swift
@@ -12,7 +12,6 @@ import Toast_Swift
 import UserNotifications
 import Fabric
 import Crashlytics
-import iRate
                
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,13 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         ToastManager.shared.style.verticalPadding = 10
         
-        //set the bundle ID. normally you wouldn't need to do this
-        //as it is picked up automatically from your Info.plist file
-        //but we want to test with an app that's actually on the store
-        iRate.sharedInstance().applicationBundleID = "com.charcoaldesign.rainbowblocks-free"
         iRate.sharedInstance().onlyPromptIfLatestVersion = false
-        
-        //enable preview mode
         iRate.sharedInstance().previewMode = true
         
         return true
--- hPig/app/controllers/hTabBarController.swift
+++ hPig/app/controllers/hTabBarController.swift
@@ -7,7 +7,77 @@
 //
 
 import UIKit
+import CoreGraphics
 
-class hTabBarController: UITabBarController {
+class hTabBarController: UITabBarController, iRateDelegate {
     
+    @IBOutlet var rateView: UIView!
+    
+    private let rateItem = iRate.sharedInstance()
+    private var alertController: UIAlertController? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Bundle.main.loadNibNamed("rate_view", owner: self, options: nil)
+        
+        rateItem?.delegate = self
+        
+        rateView.clipsToBounds = true
+        rateView.layer.cornerRadius = 8.0
+    }
+    
+    func iRateShouldPromptForRating() -> Bool {
+        
+        if let controller = presentController() {
+            let alert = UIAlertController(title: "", message: nil, preferredStyle: .alert)
+            alert.view.addSubview(rateView)
+            alert.view.translatesAutoresizingMaskIntoConstraints = false
+            rateView.translatesAutoresizingMaskIntoConstraints = false
+            
+            let views: [String: Any] = ["view" : rateView]
+            
+            alert.view.addConstraints(NSLayoutConstraint.constraints(
+                withVisualFormat: "H:|-[view(<=280)]-|",
+                options: .alignAllCenterY,
+                metrics: nil,
+                views: views))
+            
+            alert.view.addConstraints(NSLayoutConstraint.constraints(
+                withVisualFormat: "V:|-[view]-|",
+                options: .alignAllCenterX,
+                metrics: nil,
+                views: views))
+            
+            controller.present(alert, animated: true, completion: nil)
+            
+            self.alertController = alert
+        }
+        
+        return false
+    }
+    
+    private func presentController() -> UIViewController? {
+        if let navigator = self.selectedViewController as? UINavigationController,
+            let topViewController = navigator.topViewController {
+            return topViewController
+        } else {
+            return nil
+        }
+    }
+    
+    @IBAction func rateNow(_ sender: Any) {
+        if let controller = presentController() {
+            rateItem?.ratedThisVersion = true
+            rateItem?.openRatingsPageInAppStore()
+            controller.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func rateLater(_ sender: Any) {
+        if let controller = presentController() {
+            rateItem?.lastReminded = Date()
+            controller.dismiss(animated: true, completion: nil)
+        }
+    }
 }
