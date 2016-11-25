48636a2632e0c6ec2c582c4a0c866c63a6d95049
jesse.lee <jesse.lee@kakaocorp.com>
2016-11-25 13:49:16 +0900

    개발중

--- hPig/app/controllers/hTabBarController.swift
+++ hPig/app/controllers/hTabBarController.swift
@@ -15,14 +15,45 @@ class hTabBarController: UITabBarController, iRateDelegate {
     @IBOutlet weak var remindMeLaterButton: UIButton!
     @IBOutlet var customTabBar: UIView!
     
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var homeLabel: UIButton!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    @IBOutlet weak var bookmarkLabel: UIButton!
+    @IBOutlet weak var myInfoButton: UIButton!
+    @IBOutlet weak var myInfoLabel: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var settingLabel: UIButton!
+    
+    func buttons() -> [UIButton] {
+        return [homeButton, bookmarkButton, myInfoButton, settingButton]
+    }
+    
+    func labels() -> [UIButton] {
+        return [homeLabel, bookmarkLabel, myInfoLabel, settingLabel]
+    }
+    
+    private let activeButtonImages: [UIImage] = {
+        let home = #imageLiteral(resourceName: "tab_home_sel")
+        let bookmark = #imageLiteral(resourceName: "tab_book_sel")
+        let myInfo = #imageLiteral(resourceName: "tab_my_sel")
+        let setting = #imageLiteral(resourceName: "tab_set_sel")
+        return [home, bookmark, myInfo, setting]
+    }()
+    
+    private let deactiveButtonImages: [UIImage] = {
+        let home = #imageLiteral(resourceName: "tab_home_non")
+        let bookmark = #imageLiteral(resourceName: "tab_book_non")
+        let myInfo = #imageLiteral(resourceName: "tab_my_non")
+        let setting = #imageLiteral(resourceName: "tab_set_non")
+        return [home, bookmark, myInfo, setting]
+    }()
+    
     private let rateItem = iRate.sharedInstance()
     private var presentItem: UIViewController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.selectedIndex = 0
-        
+
         Bundle.main.loadNibNamed("tab_bar", owner: self, options: nil)
         Bundle.main.loadNibNamed("rate_view", owner: self, options: nil)
         
@@ -41,28 +72,32 @@ class hTabBarController: UITabBarController, iRateDelegate {
         
         remindMeLaterButton.layer.borderWidth = 1.0
         remindMeLaterButton.layer.borderColor = RGBA(252, g: 86, b: 97, a: 1.0).cgColor
+        
+        select(at: 0)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        customTabBar.frame = tabBar.frame
     }
     
     private func setupTabbar() {
-        
         customTabBar.frame = tabBar.frame
         
         self.view.addSubview(customTabBar)
         
-        self.view.translatesAutoresizingMaskIntoConstraints = false
-        customTabBar.translatesAutoresizingMaskIntoConstraints = false
+        homeButton.addTarget(self, action: #selector(self.didSelectHome(_:)), for: .touchUpInside)
+        homeLabel.addTarget(self, action: #selector(self.didSelectHome(_:)), for: .touchUpInside)
         
-        let views: [String: Any] = ["view": customTabBar]
+        bookmarkButton.addTarget(self, action: #selector(self.didSelectBookmark(_:)), for: .touchUpInside)
+        bookmarkLabel.addTarget(self, action: #selector(self.didSelectBookmark(_:)), for: .touchUpInside)
         
-        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|",
-                                                                options: .alignAllBottom,
-                                                                metrics: nil,
-                                                                views: views))
+        myInfoButton.addTarget(self, action: #selector(self.didSelectMyInfo(_:)), for: .touchUpInside)
+        myInfoLabel.addTarget(self, action: #selector(self.didSelectMyInfo(_:)), for: .touchUpInside)
         
-        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view(==59)]|",
-                                                                options: .alignAllCenterX,
-                                                                metrics: nil,
-                                                                views: views))
+        settingButton.addTarget(self, action: #selector(self.didSelectSetting(_:)), for: .touchUpInside)
+        settingLabel.addTarget(self, action: #selector(self.didSelectSetting(_:)), for: .touchUpInside)
     }
     
     func iRateShouldPromptForRating() -> Bool {
@@ -129,20 +164,50 @@ class hTabBarController: UITabBarController, iRateDelegate {
         }
     }
     
-    @IBAction func didSelectHome(_ sender: Any) {
-        self.selectedIndex = 0
+    func didSelectHome(_ sender: Any) {
+        select(at: 0)
     }
     
-    @IBAction func didSelectBookmark(_ sender: Any) {
-        self.selectedIndex = 1
+    func didSelectBookmark(_ sender: Any) {
+        select(at: 1)
         
     }
     
-    @IBAction func didSelectMyInfo(_ sender: Any) {
-        self.selectedIndex = 2
+    func didSelectMyInfo(_ sender: Any) {
+        select(at: 2)
     }
     
-    @IBAction func didSelectSetting(_ sender: Any) {
-        self.selectedIndex = 3
+    func didSelectSetting(_ sender: Any) {
+        select(at: 3)
+    }
+    
+    private func select(at index: Int) {
+        
+        if index == selectedIndex, let navigator = selectedViewController as? UINavigationController {
+            if navigator.viewControllers.count > 1 {
+                navigator.popViewController(animated: true)
+            } else {
+                navigator.setNavigationBarHidden(false, animated: false)
+            }
+            
+        }
+        
+        self.selectedIndex = index
+        
+        buttons().enumerated().forEach { (i, button) in
+            if i == index {
+                button.setImage(activeButtonImages[i], for: .normal)
+            } else {
+                button.setImage(deactiveButtonImages[i], for: .normal)
+            }
+        }
+        
+        labels().enumerated().forEach { (i, button) in
+            if i == index {
+                button.titleLabel?.textColor = RGBA(252, g: 26, b: 38, a: 1)
+            } else {
+                button.titleLabel?.textColor = RGBA(146, g: 146, b: 146, a: 1)
+            }
+        }
     }
 }
